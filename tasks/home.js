1623439fc348fed4f94729ef55efcbe8f09afc0a
lblacha <lukasz.blacha@schibsted.pl>
2015-02-17 17:32:06 +0100

    tasks fixes

--- tasks/home.js
+++ tasks/home.js
@@ -5,25 +5,33 @@ module.exports = {
         actions: ['search-autocomplete']
     },
     execute: function(pediff, config) {
-        // homepage
-        this.waitUntilVisible('.main-page-main-box img', function() {
-            this.wait(500, function(){
-                pediff.screenshot('#wrapper');
+        this.waitFor(function() {
+            return this.evaluate(function() {
+                return document.querySelectorAll('.loader').length < 1;
             });
-        });
+        }, function() {         
+            // homepage
+            this.waitUntilVisible('.main-page-main-box img', function() {
+                this.wait(500, function(){
+                    pediff.screenshot('#wrapper');
+                });
+            });
 
-        // search
-        this.then(function() {
-            this.waitForSelector('.search-form .search-input', function(){
-                this.sendKeys('.search-form .search-input', 'naan', {keepFocus: true});
-            }, function(){}, 10000);
-        });
+            // search
+            this.then(function() {
+                this.waitForSelector('.search-form .search-input', function(){
+                    this.sendKeys('.search-form .search-input', 'naan', {keepFocus: true});
+                }, function(){}, 10000);
+            });
 
-        this.then(function() {
-            // search autocomplete
-            this.waitForSelector('.search-autocomplete a', function(){
-                pediff.screenshot('#wrapper', config.actions[0]);
-            }, function(){}, 10000);
-        });
+            this.then(function() {
+                // search autocomplete
+                this.waitForSelector('.search-autocomplete a', function(){
+                    pediff.screenshot('#wrapper', config.actions[0]);
+                }, function(){}, 10000);
+            });
+        }, function() {
+            this.echo('Loaders sill spinning, giving up', 'ERROR');
+        }, 10000);
     }
 };
