a29c5943c4d41b51502a02db3f2db5de57452fcc
lblacha <lukasz.blacha@schibsted.pl>
2015-02-17 17:07:10 +0100

    tasks fixes

--- tasks/explore.js
+++ tasks/explore.js
@@ -4,8 +4,16 @@ module.exports = {
         package: 'explore'
     },
     execute: function(pediff, config) {
-        this.waitUntilVisible('#content .explore-entry img', function() {
-            pediff.screenshot('#wrapper');
-        }, function(){}, 10000);
+        this.waitFor(function() {
+            return this.evaluate(function() {
+                return document.querySelectorAll('.loader').length < 1;
+            });
+        }, function() {         
+            this.waitUntilVisible('#content .explore-entry img', function() {
+                pediff.screenshot('#wrapper');
+            }, function(){}, 10000);
+        }, function() {
+            this.echo('Loaders sill spinning, giving up', 'ERROR');
+        }, 10000);
     }
 };
